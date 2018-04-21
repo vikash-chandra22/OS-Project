0c8cc0fb8f911681061a050afb87c61fdaceda06
VIKASH CHANDRA <chandravikash18@gmail.com>
2018-04-21 12:36:33 +0530

    Update Qes 23.c

--- Qes 23.c
+++ Qes 23.c
@@ -20,3 +20,22 @@ main()
 	printf("\nEnter the number of gifts taken by Student %c%d  : ",i[j].s,i[j].n,"\n\n");
 	scanf("%d",&i[j].g);
 	}
+	int k,temp;
+	for(k=0;k<9;k++)
+	for(j=0;j<9-k;j++)
+	{
+		if(i[j].g<i[j+1].g)
+		{
+			temp=i[j].g;
+			i[j].g=i[j+1].g;
+			i[j+1].g=temp;
+		}
+	}
+	printf("\n Order of billed students according to highest priority\n");
+	for(j=0;j<10;j++)
+	{
+		printf("\n Student : %c%d      %d",i[j].s,i[j].n,i[j].g);
+	}
+	
+}
+
