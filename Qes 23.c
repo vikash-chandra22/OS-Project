c6843df9f1501d6052dacf3f6014fc449d870734
VIKASH CHANDRA <chandravikash18@gmail.com>
2018-04-21 12:35:28 +0530

    Update Qes 23.c

--- Qes 23.c
+++ Qes 23.c
@@ -6,3 +6,17 @@ struct item
 	int n;
 	int g;
 }i[10];
+main()
+{
+	int j;
+	for(j=0;j<10;j++)
+	{
+	i[j].s='s';
+	i[j].n=j+1;
+	}
+	printf("***Ramdon arrangement of gifts taken by students***\n");
+	for(j=0;j<10;j++)
+	{
+	printf("\nEnter the number of gifts taken by Student %c%d  : ",i[j].s,i[j].n,"\n\n");
+	scanf("%d",&i[j].g);
+	}
